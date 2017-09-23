89b29b4e37b1f0208b714465374ce7c38ecc67bb
espel <jakub.krupski@gmail.com>
2017-09-23 16:56:58 +0200

    asd

--- departments/src/main/java/pl/training/cloud/departments/controller/DepartmentsController.java
+++ departments/src/main/java/pl/training/cloud/departments/controller/DepartmentsController.java
@@ -2,16 +2,12 @@ package pl.training.cloud.departments.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import pl.training.cloud.common.model.Mapper;
-import pl.training.cloud.common.web.UriBuilder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import pl.training.cloud.departments.dto.DepartmentDto;
-import pl.training.cloud.departments.service.DepartmentNotFoundException;
 import pl.training.cloud.departments.service.DepartmentsService;
-
-import java.util.Locale;
 
 @Api(description = "Departments resource")
 @RequestMapping("departments")
@@ -19,12 +15,9 @@ import java.util.Locale;
 public class DepartmentsController {
 
     private DepartmentsService departmentsService;
-    private Mapper mapper;
-    private UriBuilder uriBuilder = new UriBuilder();
 
-    public DepartmentsController(DepartmentsService departmentsService, Mapper mapper) {
+    public DepartmentsController(DepartmentsService departmentsService) {
         this.departmentsService = departmentsService;
-        this.mapper = mapper;
     }
 
     @ApiOperation(value = "Get department by id", response = DepartmentDto.class)
@@ -35,10 +28,5 @@ public class DepartmentsController {
     }
 
 
-
-    @ExceptionHandler(DepartmentNotFoundException.class)
-    public ResponseEntity onOrganizationNotFound(DepartmentNotFoundException ex, Locale locale) {
-        return new ResponseEntity<>(mapper.map(ex, locale), HttpStatus.NOT_FOUND);
-    }
 
 }
